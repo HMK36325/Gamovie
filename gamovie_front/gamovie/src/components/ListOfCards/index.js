69fffeba6672f1c5b305b7e5e7a1f7f1ed5d902c
Iván Iglesias <hmk36325@educastur.es>
2022-05-07 11:46:54 +0200

    Adding front

--- gamovie_front/gamovie/src/components/ListOfCards/index.js
+++ gamovie_front/gamovie/src/components/ListOfCards/index.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { Row, Container, Col } from "react-bootstrap";
 import Card from "components/Card";
 
 export default function ListOfCards({ cards }) {
-  return cards.map(function(card) {
-    console.log(cards)
-    return <Card
-    key={card.id}
-    url={card.image}
-    year={card.year}
-    title={card.name}
-  />
-  });
-  
+  return (
+    <Container className="d-flex flex-column">
+      <Row>
+        {cards.map(function (card) {
+          return (
+            <Col
+              sm="6"
+              md="4"
+              lg="3"
+              className="d-flex justify-content-center mt-3"
+            >
+              <Card
+                key={card.id}
+                url={card.image}
+                year={card.year}
+                title={card.name}
+              />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 }
